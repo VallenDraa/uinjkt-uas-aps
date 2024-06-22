5f8086b43737dfa2817dfa1a8bf15c7a03b220a6
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-06-22 17:59:33 +0700

    remove option for COD payment

--- fashion-e-commerce/app/(front)/payment/Form.tsx
+++ fashion-e-commerce/app/(front)/payment/Form.tsx
@@ -33,7 +33,7 @@ const Form = () => {
         <div className='card-body'>
           <h1 className='card-title'>Payment Method</h1>
           <form onSubmit={handleSubmit}>
-            {['PayPal', 'CashOnDelivery'].map((payment) => (
+            {['PayPal'].map((payment) => (
               <div key={payment}>
                 <label className='label cursor-pointer'>
                   <span className='label-text'>{payment}</span>
